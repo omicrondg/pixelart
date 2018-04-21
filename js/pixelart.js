88a5e63ca8cf70d97285351b01e71a939efd9039
OmicronDG <omicron_dg@mail.ru>
2018-04-21 22:04:23 +0300

    Add clear grid methos

--- js/pixelart.js
+++ js/pixelart.js
@@ -1,30 +1,33 @@
-let grid = document.querySelector('.pixels');
+const grid = document.querySelector('#pixelsGrid');
 const width = document.querySelector('#inputWidth');
 const height = document.querySelector('#inputHeight');
 const picker = document.querySelector('#colorPicker');
 const form = document.querySelector('#sizePicker');
-
+const createBtn = document.querySelector('#createGrid');
+const clearBtn = document.querySelector('#clearGrid');
 
 let rows = Number(height.value);
 let columns = Number(width.value);
 let isGrid = false;
 
 let color = picker.value;
-let table = document.querySelector('table');
 
-let defaultColor = table.style.backgroundColor;
 
-function MakeGrid(rows=5, columns=5){
+let defaultColor;
+
+function MakeGrid() {
 
   for (let i = 0; i < rows; i++) {
-    var tRow = document.createElement('tr');
+    let tRow = document.createElement('tr');
     for (let j = 0; j < columns; j++) {
-      var tColumn = document.createElement('td');
+      let tColumn = document.createElement('td');
       tRow.appendChild(tColumn);
     }
-    table.appendChild(tRow);
+    grid.appendChild(tRow);
   }
   isGrid = true;
+
+  defaultColor = grid.style.backgroundColor;
 }
 
 form.addEventListener("change", event => {
@@ -32,47 +35,35 @@ form.addEventListener("change", event => {
   columns = Number(width.value);
 });
 
-form.addEventListener("submit", event =>
-{ 
+form.addEventListener("submit", event => {
+  const clearBtn = document.querySelector('#clearGrid');
   if (isGrid) {
-    rows = Number(height.value) + GetCurrentRowsCount();
-    columns = Number(width.value) + GetCurrentCellsCount();
-    MakeGrid(rows, columns);
+    clearGrid();
+    MakeGrid();
     event.preventDefault();
   } else {
-    MakeGrid(rows, columns);
+    MakeGrid();
     event.preventDefault();
   }
+
 });
-  
+
 picker.addEventListener("change", event => {
   color = picker.value;
 });
 
-table.addEventListener("click", event => {
+grid.addEventListener("click", event => {
   if (event.target.nodeName.toLowerCase() === 'td')
     (event.target).style.backgroundColor = color;
 });
 
-table.addEventListener("dblclick", event => {
+grid.addEventListener("dblclick", event => {
   if (event.target.nodeName.toLowerCase() === 'td')
-  (event.target).style.backgroundColor = defaultColor;
+    (event.target).style.backgroundColor = defaultColor;
 });
 
-function GetCurrentRowsCount(){
-  let rowsCount = 0;
-  table.forEach(element => {
-    if(element.nodeName.toLowerCase() == 'tr')
-    rowsCount++;
-  });
-  return rowsCount;
-};
-
-function GetCurrentCellsCount() {
-  let cellsCount = 0;
-  table.forEach(element => {
-    if (element.nodeName.toLowerCase() == 'td')
-      cellsCount++;
-  });
-  return cellsCount;
+function clearGrid() {
+  while (grid.childNodes.length != 0) {
+    grid.childNodes.forEach(e => e.remove());
+  }
 }
